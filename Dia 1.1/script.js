1bf8f1192033ea0f064f2a6a84f8a49802510793
Jessica Diniz <jessicadinizcontato@gmail.com>
2021-06-08 17:44:21 -0300

    Delete Dia 1.1 directory

--- Dia 1.1/script.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-function cont() {
-  count+=1;
-  document.getElementsByClassName("contador")[0].innerHTML = count;
-   if(count > 4){
-       alert("ATENÇÃO: 4 É O NÚMERO MÁXIMO DE INTEGRANTES DO SQUAD!!")
-        document.getElementsByClassName('contador')[0].innerHTML = 4;
-   }
- }

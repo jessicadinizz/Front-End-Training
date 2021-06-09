ba32674a2bdf2f90435191b6c06951028c0780f2
Thiago <thiagobastos71@gmail.com>
2021-06-09 15:54:53 -0300

    faz alterações de acordo com o feedback

--- Dia-2.1/script.js
+++ Dia-2.1/script.js
@@ -1,19 +1,21 @@
 function somar() {
-    let tn1 = document.getElementById("n1");
-    let tn2 = document.getElementById("n2");
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 + n2;
-    document.getElementById("sumRes").value = s;
+    let sumtn1 = document.getElementById("sum-number1");
+    let sumtn2 = document.getElementById("sum-number2");
+    let sum1 = Number(sumtn1.value);
+    let sum2 = Number(sumtn2.value);
+    let s = sum1 + sum2;
+    document.getElementById("sumResult").value = s;
+    
 }
 
 function subtrair() {
-    let subtn1 = document.getElementById("subn1");
-    let subtn2 = document.getElementById("subn2");
+    let subtn1 = document.getElementById("sub-number1");
+    let subtn2 = document.getElementById("sub-number2");
     let sub1 = Number(subtn1.value);
     let sub2 = Number(subtn2.value);
     sub = sub1 - sub2;
-    document.getElementById("subRes").value = sub;
+    document.getElementById("subResult").value = sub;
+
 }
 
 function multiplicar() {
@@ -22,18 +24,19 @@ function multiplicar() {
     let mult1 = Number(multtn1.value);
     let mult2 = Number(multtn2.value);
     mult = mult1 * mult2;
-    document.getElementById("multRes").value = mult;
+    document.getElementById("multResult").value = mult;
+    
 }
 
 function dividir() {
-    let tn1111 = document.getElementById("n7");
-    let tn2222 = document.getElementById("n8");
-    let n7 = Number(tn1111.value);
-    let n8 = Number(tn2222.value);
-    let di = n7 / n8;
-    if (n8 == 0) {
+    let divisiontn1 = document.getElementById("division-number1");
+    let divisiontn2 = document.getElementById("division-number2");
+    let division1 = Number(divisiontn1.value);
+    let division2 = Number(divisiontn2.value);
+    let division = division1 / division2;
+    if (division2 == 0) {
         alert("Essa divisão não existe");
-        di = null;
+        division = null;
     }
-    document.getElementById("diRes").value = di;
+    document.getElementById("divisionResult").value = division;
 }

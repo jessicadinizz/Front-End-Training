9477701f943d059bc5e9f27541af5ef08aead0b3
Jessica Diniz <jessica.diniz5566@gmail.com>
2021-06-10 23:34:23 -0300

    Alterações conforme o feedback

---
+++ Dia-3.1/meu-app2/src/App.js
@@ -0,0 +1,20 @@
+import Head from "./components/header";
+import Foot from "./components/footer";
+import Showhidden from "./components/showhidden";
+
+
+function App() {
+  return (
+    <div className="wrapper-app">
+
+        <Head />
+
+        <Showhidden />
+
+        <Foot />
+
+    </div>
+  );
+}
+
+export default App;

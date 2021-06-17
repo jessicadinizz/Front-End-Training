cf628d0f919fb0d98dddb745b41f9de44e06dda0
Pedro Longue <pedrolongue13@gmail.com>
2021-06-17 14:03:48 -0300

    Alterações de acordo com o feedback

--- Dia-4.0/my-app3/src/components/ToDo/index.jsx
+++ Dia-4.0/my-app3/src/components/ToDo/index.jsx
@@ -1,71 +1,45 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./styles.css";
 
-class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: "" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+import React, { useState } from "react";
 
-  render() {
-    return (
-      <div className="wapper-list">
-        <h1 className="title-list">Lista</h1>
+function List() {
+  const [newTask, setNewTask] = useState("");
+  const [items, setItems] = useState([]);
 
-        <form className="form-list" onSubmit={this.handleSubmit}>
-          <input
-            className="input-list"
-            placeholder="Nome"
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-
-          <button className="button-list">Adicionar</button>
-        </form>
-        <TodoList items={this.state.items} />
-      </div>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
+  function handleNewTask(e) {
     e.preventDefault();
-    if (this.state.text.length === 0) {
-      alert("Preencha o campo com algo válido");
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now(),
-    };
-    this.setState((state) => ({
-      items: state.items.concat(newItem),
-      text: "",
-    }));
+    setItems((prevState) => {
+      {
+        setNewTask("");
+      }
+      return [newTask, ...prevState];
+    });
   }
-}
 
-class TodoList extends React.Component {
-  render() {
-    return (
+  return (
+    <div className="wapper-list">
+      <h1 className="title-list">Lista</h1>
+      <form className="form-list" onSubmit={handleNewTask}>
+        <input
+          className="input-list"
+          placeholder="Nome"
+          id="new-todo"
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
+
+        <button className="button-list">Adicionar</button>
+      </form>
+
       <ul className="items-list">
-        {this.props.items.map((item) => (
-          <p key={item.id}>
-            <strong>Tarefa</strong>:{item.text}
+        {items.map((item) => (
+          <p key={item}>
+            <strong>Tarefa</strong>:{item}
           </p>
         ))}
       </ul>
-    );
-  }
+    </div>
+  );
 }
-
-ReactDOM.render(<List />, document.getElementById("root"));
 
 export default List;

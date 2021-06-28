61ceb01e21829e345ddba77405f077ab877efa4f
Pedro Longue <pedrolongue13@gmail.com>
2021-06-28 15:34:38 -0300

    Finaliza o site

--- Dia-6.0/my-app4/src/components/Cards/index.jsx
+++ Dia-6.0/my-app4/src/components/Cards/index.jsx
@@ -1,87 +1,96 @@
 import "./styles.css";
 import React, { useState } from "react";
+import Lixeira from "../../lixeira.png";
 
 function List() {
   const [newName, setNewName] = useState("");
-  const [Name, setName] = useState([]);
   const [newTime, setNewTime] = useState("");
-  const [Time, setTime] = useState([]);
   const [newClass, setNewClass] = useState("");
-  const [Class, setClass] = useState([]);
+  const [Item, setItem] = useState([]);
 
-  function handleNew(e) {
+  function addNew(e) {
     e.preventDefault();
-    setName((prevState) => {
-      {
-        setNewName("");
-      }
-      return [newName, ...prevState];
+    if (!newName || !newTime || !newClass){
+      return alert('Por favor, preencha todos os campos!')
+    }
+
+    const newObj = {
+      name: newName,
+      time: newTime,
+      category: newClass,
+      id: Math.random(),
+    };
+
+    setItem([...Item, newObj]);
+    setNewName("")
+    setNewTime("")
+    setNewClass("")
+  }
+
+  function removeItem(id) {
+    const filteredNewListItem = Item.filter(function (items) {
+      return items.id !== id;
     });
-    e.preventDefault();
-    setTime((prevState) => {
-      {
-        setNewTime("");
-      }
-      return [newTime, ...prevState];
-    });
-    e.preventDefault();
-    setClass((prevState) => {
-      {
-        setNewClass("");
-      }
-      return [newClass, ...prevState];
-    });
+
+    setItem(filteredNewListItem);
   }
 
   return (
     <div className="wapper-list">
       <div className="title-and-form">
         <h1 className="title-list">Receitas</h1>
-        <form className="form-list" onSubmit={handleNew}>
-          <input
-            className="input-list"
-            placeholder="Nome"
-            id="new-Name"
-            onChange={(e) => setNewName(e.target.value)}
-            value={newName}
-          />
-          <input
-            className="input-list"
-            placeholder="Tempo de Preparo"
-            id="new-Time"
-            onChange={(e) => setNewTime(e.target.value)}
-            value={newTime}
-          />
-          <input
-            className="input-list"
-            placeholder="Categoria"
-            id="new-Class"
-            onChange={(e) => setNewClass(e.target.value)}
-            value={newClass}
-          />
+          <form className="form-list" onSubmit={addNew}>
+            <input
+              className="input-list"
+              type="text"
+              placeholder="Nome"
+              id="new-Name"
+              onChange={(e) => setNewName(e.target.value)}
+              value={newName}
+            />
+            <input
+              className="input-list"
+              type="text"
+              placeholder="Tempo de preparo"
+              id="new-Time"
+              onChange={(e) => setNewTime(e.target.value)}
+              value={newTime}
+            />
+            <input
+              className="input-list"
+              type="text"
+              placeholder="Categoria"
+              id="new-Class"
+              onChange={(e) => setNewClass(e.target.value)}
+              value={newClass}
+            />
 
-          <button className="button-list">Adicionar</button>
-        </form>
+            <button className="button-list">
+              Adicionar
+            </button>
+          </form>
       </div>
 
       <ul>
         <div className="list-contents">
-          {Name.map((nome) => (
-            <p className="items-list" key={nome}>
-              <span>{nome}</span>
-            </p>
-          ))}
-          {Time.map((tempo) => (
-            <p className="items-list" key={tempo}>
-              <span>{tempo}</span>
-            </p>
-          ))}
-
-          {Class.map((classe) => (
-            <p className="items-list" key={classe}>
-              <span>{classe}</span>
-            </p>
-          ))}
+          {Item.map((item) => {
+            return (
+              <div className="items-list" key={item}>
+                <div className="img-form">
+                  <button className="erase-button-list" onClick={() => removeItem(item.id)}>
+                    <img className="erase-list" src={Lixeira}></img>
+                  </button>
+                </div>
+                <div className="first-item">
+                  <p className="p-name-item">{item.name}</p>
+                </div>
+                <div className="second-item">
+                  <p className="p-item">{item.time}</p>
+                  <p className="p-item">{item.category}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </ul>
     </div>
